688b6a8d244d502c2692709bec85a581830e02a8
wishlucknow <wishlucknow@gmail.com>
2025-05-01 16:25:38 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,5 @@
+<?php
+// $conn= mysqli_connect("localhost","root","","famillytree");
+$conn = mysqli_connect("localhost","u800183464_familyhub","Nf1US9:b*","u800183464_familyhub");
+?>                                      
+
